878105f08c07163196402e74d154c286368979c4
MAYRA DE CASTILHOS ALVES <ALU201715865@uniritter.local>
2019-05-14 21:43:23 -0300

    Terceira mensagem

--- teste1.java
+++ teste1.java
@@ -3,7 +3,8 @@ class Teste1 {
 	public static void main (String) main{
 		
 		system.out.println("Olá mundo");
-		system.out.println("Olá mundo");
+		system.out.println("Mensagem 2");
+		system.out.println("outra mensagem");
 	
 	}
 
